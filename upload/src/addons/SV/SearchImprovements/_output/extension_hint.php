8363ef8457e4b43c250ac4393c714cc22bac8ff7
Xon <tazipper1@hotmail.com>
2018-11-24 01:27:37 +0800

    Initial add-on creation

---
+++ upload/src/addons/SV/SearchImprovements/_output/extension_hint.php
@@ -0,0 +1,19 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\SearchImprovements\XFES\Search\Source
+{
+	class XFCP_Elasticsearch extends \XFES\Search\Source\Elasticsearch {}
+}
+
+namespace SV\SearchImprovements\XF\Search\Query
+{
+	class XFCP_Query extends \XF\Search\Query\Query {}
+}
+
+namespace SV\SearchImprovements\XF\Search\Source
+{
+	class XFCP_MySqlFt extends \XF\Search\Source\MySqlFt {}
+}
