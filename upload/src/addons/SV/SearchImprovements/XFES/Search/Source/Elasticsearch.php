aecd762abc61a4f147ec6d43afadb8d03e8bc08e
Xon <tazipper1@hotmail.com>
2018-11-24 14:03:18 +0800

    Stub-in weightByContentType

--- upload/src/addons/SV/SearchImprovements/XFES/Search/Source/Elasticsearch.php
+++ upload/src/addons/SV/SearchImprovements/XFES/Search/Source/Elasticsearch.php
@@ -65,9 +65,62 @@ class Elasticsearch extends XFCP_Elasticsearch
     {
         if (\XF::options()->searchImpov_simpleQuerySyntax)
         {
-            return str_replace('/','\/', $keywords);
+            return str_replace('/', '\/', $keywords);
         }
 
         return parent::parseKeywords($keywords, $error, $warning);
+    }
+
+    protected function getDslFromQuery(Query\Query $query, $maxResults)
+    {
+        $dsl = parent::getDslFromQuery($query, $maxResults);
+
+        // skip specific type handler searches
+        // only support ES > 1.2 & relevance weighting or plain sorting by relevance score
+        if (!$query->getHandlerType() &&
+            isset($dsl['sort'][0]['_score']) ||
+            isset($dsl['query']['function_score']) ||
+            isset($dsl['query']['bool']['must']['function_score']))
+        {
+            $this->weightByContentType($dsl);
+        }
+
+        return $dsl;
+    }
+
+    function weightByContentType(array &$dsl)
+    {
+        // pre content type weighting
+        $contentTypeWeighting = \XF::options()->content_type_weighting;
+        if (!$contentTypeWeighting || !is_array($contentTypeWeighting))
+        {
+            return;
+        }
+
+        $functions = [];
+        $isSingleTypeIndex = $this->es->isSingleTypeIndex();
+        foreach ($contentTypeWeighting as $contentType => $weight)
+        {
+            if ($weight == 1)
+            {
+                continue;
+            }
+            $functions[] = [
+                "filter" => $isSingleTypeIndex ? ['term' => ['type' => $contentType]] : ['type' => ['value' => $contentType]],
+                "weight" => $weight
+            ];
+        }
+
+        if (!$functions)
+        {
+            return;
+        }
+
+        $dsl['query'] = [
+            'function_score' => [
+                'query'     => $dsl['query'],
+                'functions' => $functions
+            ]
+        ];
     }
 }
