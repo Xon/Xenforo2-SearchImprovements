d7023ed77e09b315824c5ce2f8dc2e5b8f5bfae7
Xon <tazipper1@hotmail.com>
2018-11-24 13:07:43 +0800

    Implement esLogDSL option, remove esLogDSLOnError option

--- upload/src/addons/SV/SearchImprovements/XFES/Elasticsearch/Api.php
+++ upload/src/addons/SV/SearchImprovements/XFES/Elasticsearch/Api.php
@@ -2,8 +2,6 @@
 
 namespace SV\SearchImprovements\XFES\Elasticsearch;
 
-
-
 /**
  * Extends \XFES\Elasticsearch\Api
  */
@@ -12,5 +10,14 @@ class Api extends XFCP_Api
     public function getClusterInfo()
     {
         return $this->request('get', "/_cluster/health", null)->getBody();
+    }
+
+    public function search(array $dsl)
+    {
+        if (\XF::options()->esLogDSL)
+        {
+            \XF::logError(\json_encode($dsl));
+        }
+        return parent::search($dsl);
     }
 }
