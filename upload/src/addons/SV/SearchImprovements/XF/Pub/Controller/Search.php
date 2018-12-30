b7681d5968fa6ccefd0e6579f244c302ef2f3361
Xon <tazipper1@hotmail.com>
2018-12-31 04:03:29 +0800

    Respect per-user default search order when viewing the advanced search page

--- upload/src/addons/SV/SearchImprovements/XF/Pub/Controller/Search.php
+++ upload/src/addons/SV/SearchImprovements/XF/Pub/Controller/Search.php
@@ -2,7 +2,8 @@
 
 namespace SV\SearchImprovements\XF\Pub\Controller;
 
-
+use XF\Mvc\ParameterBag;
+use XF\Mvc\Reply\View;
 
 /**
  * Extends \XF\Pub\Controller\Search
@@ -10,6 +11,24 @@ namespace SV\SearchImprovements\XF\Pub\Controller;
 class Search extends XFCP_Search
 {
     protected $shimOrder = null;
+
+    public function actionIndex(ParameterBag $params)
+    {
+        $reply = parent::actionIndex($params);
+
+        if ($reply instanceof View)
+        {
+            $input = $reply->getParam('input');
+            /** @var \SV\SearchImprovements\XF\Entity\User $visitor */
+            $visitor = \XF::visitor();
+            if ($visitor->canChangeSearchOptions() && $visitor->Option->sv_default_search_order && empty($input['order']))
+            {
+                $reply->setParam('input', ['order' => $visitor->Option->sv_default_search_order]);
+            }
+        }
+
+        return $reply;
+    }
 
     public function actionSearch()
     {
