8566eb80574d81a58169b5a3017da74ef2682970
Xon <tazipper1@hotmail.com>
2020-10-20 06:50:15 +0800

    Compatibility update when resolving an extended class to the root class

--- upload/src/addons/SV/SearchImprovements/XF/Search/Source/MySqlFt.php
+++ upload/src/addons/SV/SearchImprovements/XF/Search/Source/MySqlFt.php
@@ -2,11 +2,9 @@
 
 namespace SV\SearchImprovements\XF\Search\Source;
 
-if (\XF::$versionId < 2020000)
-{
-    \class_alias('SV\SearchImprovements\XF\Search\Source\XF2\MySqlFt', 'SV\SearchImprovements\XF\Search\Source\MySqlFt');
-}
-else
-{
-    \class_alias('SV\SearchImprovements\XF\Search\Source\XF22\MySqlFt', 'SV\SearchImprovements\XF\Search\Source\MySqlFt');
-}
+\SV\StandardLib\Helper::repo()->aliasClass(
+    'SV\SearchImprovements\XF\Search\Source\MySqlFt',
+    \XF::$versionId < 2020000
+        ? 'SV\SearchImprovements\XF\Search\Source\XF2\MySqlFt'
+        : 'SV\SearchImprovements\XF\Search\Source\XF22\MySqlFt'
+);
