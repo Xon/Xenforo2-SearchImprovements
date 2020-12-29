8afb78ed43b369f0855ddee2da428b78d8767631
Xon <tazipper1@hotmail.com>
2020-12-30 06:05:48 +0800

    Re-add "On error, log search DSL" option to aid troubleshooting of unexpectedly failing search queries

--- upload/src/addons/SV/SearchImprovements/XFES/Elasticsearch/Api.php
+++ upload/src/addons/SV/SearchImprovements/XFES/Elasticsearch/Api.php
@@ -7,20 +7,43 @@ namespace SV\SearchImprovements\XFES\Elasticsearch;
  */
 class Api extends XFCP_Api
 {
+    /** @var array|null s*/
+    protected $dslForError;
+
+    /** @noinspection PhpMissingReturnTypeInspection */
     public function getClusterInfo()
     {
         return $this->request('get', "/_cluster/health", null)->getBody();
     }
 
+    /** @noinspection PhpMissingReturnTypeInspection */
     public function search(array $dsl)
     {
-        if (\XF::options()->esLogDSL)
+        $esLogDSL = \XF::options()->esLogDSL ?? false;
+        if ($esLogDSL)
         {
+            $this->dslForError = null;
             \XF::logError(\json_encode($dsl));
         }
-        return parent::search($dsl);
+        else
+        {
+            $this->dslForError = $dsl;
+        }
+        try
+        {
+            return parent::search($dsl);
+        }
+        finally
+        {
+            $this->dslForError = null;
+        }
     }
 
+    /**
+     * @param array $body
+     * @return string|null
+     * @noinspection PhpMissingReturnTypeInspection
+     */
     protected function getErrorMessage(array $body)
     {
         $reason = parent::getErrorMessage($body);
@@ -33,6 +56,13 @@ class Api extends XFCP_Api
             return strval($body['error']['failed_shards'][0]['reason']['type']) . ": " .
                    strval($body['error']['failed_shards'][0]['reason']['caused_by']['type']) . " " .
                    strval($body['error']['failed_shards'][0]['reason']['caused_by']['reason']);
+        }
+
+        // log DSL on error
+        $esLogDSLOnError = \XF::options()->esLogDSLOnError ?? true;
+        if ($esLogDSLOnError && $this->dslForError)
+        {
+            $reason = ($reason ? $reason . "\n " : '') . "DSL:" . \json_encode($this->dslForError);
         }
 
         return $reason;
