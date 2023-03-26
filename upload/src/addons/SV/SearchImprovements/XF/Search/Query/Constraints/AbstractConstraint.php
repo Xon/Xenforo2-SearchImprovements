1aa20d9d83e65e53a61a25f24ed3af2b5a49e278
Xon <635541+Xon@users.noreply.github.com>
2023-03-26 21:38:07 +0800

    Remove nulls from values by default for these typed constraints

--- upload/src/addons/SV/SearchImprovements/XF/Search/Query/Constraints/AbstractConstraint.php
+++ upload/src/addons/SV/SearchImprovements/XF/Search/Query/Constraints/AbstractConstraint.php
@@ -3,15 +3,46 @@
 namespace SV\SearchImprovements\XF\Search\Query\Constraints;
 
 use SV\SearchImprovements\Search\MetadataSearchEnhancements;
+use XF\Mvc\Entity\AbstractCollection;
 use XF\Search\Query\MetadataConstraint;
 use XF\Search\Query\SqlConstraint;
 use XFES\Search\Source\Elasticsearch;
 use function array_filter;
 use function count;
+use function is_array;
 use function reset;
 
 abstract class AbstractConstraint extends MetadataConstraint
 {
+    /**
+     * @param string $key
+     * @param mixed  $values
+     * @param string $matchType
+     */
+    public function __construct(string $key, $values, string $matchType)
+    {
+        parent::__construct($key, $values, $matchType);
+    }
+
+    public function setValues($values)
+    {
+        if ($values instanceof AbstractCollection)
+        {
+            $values = $values->toArray();
+        }
+
+        if (!is_array($values))
+        {
+            $values = [$values];
+        }
+
+        $values = array_filter($values, function ($v): bool {
+            return $v !== null;
+        });
+
+        $this->values = $values;
+    }
+
     abstract protected function getAllowedMatchTypes(): array;
 
     /**
@@ -51,10 +82,8 @@ abstract class AbstractConstraint extends MetadataConstraint
      */
     protected function processChildConstraints(Elasticsearch $source, array &$childFilters, array &$childNotFilters): int
     {
-        /** @var array<MetadataConstraint|null> $constraints */
-        $constraints = array_filter($this->getValues(), function ($v): bool {
-            return $v !== null;
-        });
+        /** @var array<MetadataConstraint> $constraints */
+        $constraints = $this->getValues();
         if (count($constraints) === 0)
         {
             return 0;
