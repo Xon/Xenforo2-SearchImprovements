b86f8e8a043154be7972d0b20d62c5431598ba30
Xon <635541+Xon@users.noreply.github.com>
2023-04-29 04:32:46 +0800

    Move SearchEntity::sv_debug_info setup code into SearchEntity::setupFromQuery as the more logical place to put it, and simplifies setupFromQuery and remove a potentially incorrect check on a 'recent' query

--- upload/src/addons/SV/SearchImprovements/XF/Entity/Search.php
+++ upload/src/addons/SV/SearchImprovements/XF/Entity/Search.php
@@ -2,6 +2,8 @@
 
 namespace SV\SearchImprovements\XF\Entity;
 
+use SV\SearchImprovements\Globals;
+use SV\SearchImprovements\XF\Entity\Search as SearchEntity;
 use SV\SearchImprovements\XF\Repository\Search as SearchRepo;
 use XF\Mvc\Entity\Manager;
 use XF\Mvc\Entity\Structure;
@@ -9,6 +11,7 @@ use XF\Util\Arr;
 use function array_diff;
 use function array_key_exists;
 use function array_merge;
+use function array_merge_recursive;
 use function arsort;
 use function assert;
 use function count;
@@ -16,6 +19,9 @@ use function in_array;
 use function is_array;
 use function is_numeric;
 use function is_string;
+use function memory_get_peak_usage;
+use function microtime;
+use function round;
 
 /**
  * @property array|null $sv_debug_info
@@ -505,6 +511,67 @@ class Search extends XFCP_Search
         return $handler;
     }
 
+
+    protected function getSearchDebugRequestStateSnapshot(): array
+    {
+        $debug = [
+            'time' => round(microtime(true) - $this->app()->container('time.granular'), 4),
+            'queries' => $this->db()->getQueryCount(),
+            'memory' => round(memory_get_peak_usage() / 1024 / 1024, 2)
+        ];
+
+        if (\XF::isAddOnActive('SV/RedisCache'))
+        {
+            $mainConfig = \XF::app()->config()['cache'];
+            $contexts = [];
+            $contexts[''] = $mainConfig;
+            if (isset($mainConfig['context']))
+            {
+                $contexts = $contexts + $mainConfig['context'];
+            }
+            foreach ($contexts as $contextLabel => $config)
+            {
+                $cache = \XF::app()->cache($contextLabel, false);
+                if ($cache instanceof \SV\RedisCache\Redis)
+                {
+                    $stats = $cache->getRedisStats();
+                    if (!isset($debug['cache']['get'], $debug['cache']['set']))
+                    {
+                        $debug['cache']['get'] = 0;
+                        $debug['cache']['set'] = 0;
+                    }
+                    $debug['cache']['get'] += (int)($stats['gets'] ?? 0);
+                    $debug['cache']['set'] += (int)($stats['sets'] ?? 0);
+                }
+            }
+        }
+
+        return $debug;
+    }
+
+    protected function getSearchDebugSummary(SearchEntity $search): array
+    {
+        $arr = [
+            'summary' => [],
+        ];
+
+        foreach ($search->search_results as $match)
+        {
+            $contentType = $match[0] ?? null;
+            if (!is_string($contentType))
+            {
+                throw new \LogicException('Unknown return contents from Search::search_results');
+            }
+            if (!isset($arr['summary'][$contentType]['php']))
+            {
+                $arr['summary'][$contentType]['php'] = 0;
+            }
+            $arr['summary'][$contentType]['php'] += 1;
+        }
+
+        return $arr;
+    }
+
     public function setupFromQuery(\XF\Search\Query\KeywordQuery $query, array $constraints = [])
     {
         parent::setupFromQuery($query, $constraints);
@@ -526,6 +593,21 @@ class Search extends XFCP_Search
                 $this->search_constraints = $constraints;
                 $this->search_type = $firstChildType;
             }
+        }
+
+        // Log debug information if required.
+        // This requires setup inside runSearch as by the time setupFromQuery is called the search DSL and other bits can't be collected
+        $capturedSearchDebugInfo = Globals::$capturedSearchDebugInfo ?? [];
+        // convert empty array to null
+        if ($capturedSearchDebugInfo !== [])
+        {
+            $capturedSearchDebugInfo = array_merge_recursive(
+                $capturedSearchDebugInfo,
+                $this->getSearchDebugRequestStateSnapshot(),
+                $this->getSearchDebugSummary($this)
+            );
+
+            $this->sv_debug_info = $capturedSearchDebugInfo;
         }
     }
 
--- upload/src/addons/SV/SearchImprovements/XF/Repository/Search.php
+++ upload/src/addons/SV/SearchImprovements/XF/Repository/Search.php
@@ -5,12 +5,10 @@ namespace SV\SearchImprovements\XF\Repository;
 use SV\SearchImprovements\Globals;
 use SV\SearchImprovements\XF\Entity\Search as SearchEntity;
 use function array_diff;
-use function array_merge_recursive;
 use function assert;
 use function count;
 use function in_array;
 use function is_array;
-use function is_string;
 use function reset;
 
 /**
@@ -69,66 +67,6 @@ class Search extends XFCP_Search
         return null;
     }
 
-    protected function getSearchDebugRequestStateSnapshot(): array
-    {
-        $debug = [
-            'time' => round(microtime(true) - $this->app()->container('time.granular'), 4),
-            'queries' => $this->db()->getQueryCount(),
-            'memory' => round(memory_get_peak_usage() / 1024 / 1024, 2)
-        ];
-
-        if (\XF::isAddOnActive('SV/RedisCache'))
-        {
-            $mainConfig = \XF::app()->config()['cache'];
-            $contexts = [];
-            $contexts[''] = $mainConfig;
-            if (isset($mainConfig['context']))
-            {
-                $contexts = $contexts + $mainConfig['context'];
-            }
-            foreach ($contexts as $contextLabel => $config)
-            {
-                $cache = \XF::app()->cache($contextLabel, false);
-                if ($cache instanceof \SV\RedisCache\Redis)
-                {
-                    $stats = $cache->getRedisStats();
-                    if (!isset($debug['cache']['get'], $debug['cache']['set']))
-                    {
-                        $debug['cache']['get'] = 0;
-                        $debug['cache']['set'] = 0;
-                    }
-                    $debug['cache']['get'] += (int)($stats['gets'] ?? 0);
-                    $debug['cache']['set'] += (int)($stats['sets'] ?? 0);
-                }
-            }
-        }
-
-        return $debug;
-    }
-
-    protected function getSearchDebugSummary(SearchEntity $search): array
-    {
-        $arr = [
-            'summary' => [],
-        ];
-
-        foreach ($search->search_results as $match)
-        {
-            $contentType = $match[0] ?? null;
-            if (!is_string($contentType))
-            {
-                throw new \LogicException('Unknown return contents from Search::search_results');
-            }
-            if (!isset($arr['summary'][$contentType]['php']))
-            {
-                $arr['summary'][$contentType]['php'] = 0;
-            }
-            $arr['summary'][$contentType]['php'] += 1;
-        }
-
-        return $arr;
-    }
-
     public function runSearch(\XF\Search\Query\KeywordQuery $query, array $constraints = [], $allowCached = true)
     {
         if (\XF::options()->svShowSearchDebugInfo ?? '')
@@ -139,34 +77,14 @@ class Search extends XFCP_Search
         {
             $search = parent::runSearch($query, $constraints, $allowCached);
 
-            // re-used search, don't bother updating the search debug info
-            if ($search !== null && $search->search_date < \XF::$time)
-            {
-                return $search;
-            }
-
             if ($search === null)
             {
                 $search = $this->em->create('XF:Search');
                 assert($search instanceof SearchEntity);
                 $search->setupFromQuery($query, $constraints);
                 $search->user_id = \XF::visitor()->user_id;
+                $search->save();
             }
-            assert($search instanceof SearchEntity);
-
-            $capturedSearchDebugInfo = Globals::$capturedSearchDebugInfo ?? [];
-            // convert empty array to null
-            if ($capturedSearchDebugInfo !== [])
-            {
-                $capturedSearchDebugInfo = array_merge_recursive(
-                    $capturedSearchDebugInfo,
-                    $this->getSearchDebugRequestStateSnapshot(),
-                    $this->getSearchDebugSummary($search)
-                );
-
-                $search->sv_debug_info = $capturedSearchDebugInfo;
-            }
-            $search->saveIfChanged();
 
             return $search;
         }
