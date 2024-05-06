1db497e5bb28e9768080468074813f6e42e6919a
Xon <635541+Xon@users.noreply.github.com>
2024-05-06 08:45:33 +0800

    Push prefix text into the search index (using the site's default language)

--- upload/src/addons/SV/SearchImprovements/XF/Search/Data/Thread.php
+++ upload/src/addons/SV/SearchImprovements/XF/Search/Data/Thread.php
@@ -6,12 +6,63 @@
 namespace SV\SearchImprovements\XF\Search\Data;
 
 use SV\SearchImprovements\Search\DiscussionTrait;
+use XF\Mvc\Entity\Entity;
+use XF\Search\IndexRecord;
 use XF\Search\MetadataStructure;
 
 class Thread extends XFCP_Thread
 {
     protected static $svDiscussionEntity = \XF\Entity\Thread::class;
     use DiscussionTrait;
+
+    public function getIndexData(Entity $entity)
+    {
+        /** @var \XF\Entity\Thread $entity */
+        $index = parent::getIndexData($entity);
+
+        if ($index !== null)
+        {
+            $this->svIndexPrefixes($entity, $index);
+        }
+
+        return $index;
+    }
+
+    protected function svIndexPrefixes(\XF\Entity\Thread $thread, IndexRecord $index): void
+    {
+        $prefixIds = [];
+        if (\XF::isAddOnActive('SV/MultiPrefix') && $thread->isValidColumn('sv_prefix_ids'))
+        {
+            /** @var \SV\MultiPrefix\XF\Entity\Thread $thread */
+            $prefixIds = $thread->sv_prefix_ids;
+        }
+        else if ($thread->prefix_id)
+        {
+            $prefixIds = [$thread->prefix_id];
+        }
+
+        $language = \XF::app()->language();
+        $prefixes = [];
+        foreach ($prefixIds AS $prefixId)
+        {
+            $prefixId = (int)$prefixId;
+            $key = 'thread_prefix.' . $prefixId;
+
+            $phraseText = trim((string)$language->renderPhrase($key, [], 'raw', [
+                'fallback' => null,
+                'nameOnInvalid' => false,
+            ]));
+            if ($phraseText !== '')
+            {
+                $prefixes[$prefixId] = $phraseText;
+            }
+        }
+
+        if (count($prefixes) !== 0)
+        {
+            $index->title .= ' ' . implode(' ', $prefixes);
+        }
+    }
 
     protected function getMetaData(\XF\Entity\Thread $entity)
     {
