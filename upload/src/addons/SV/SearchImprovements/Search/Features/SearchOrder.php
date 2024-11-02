0a87bc0329be0cdf818b008a0d513a4d20e2b1fc
Xon <635541+Xon@users.noreply.github.com>
2024-11-02 14:27:28 +0800

    XF2.3 compatibility fix for custom "SearchOrder" class

--- upload/src/addons/SV/SearchImprovements/Search/Features/SearchOrder.php
+++ upload/src/addons/SV/SearchImprovements/Search/Features/SearchOrder.php
@@ -2,16 +2,19 @@
 
 namespace SV\SearchImprovements\Search\Features;
 
+use XF\Search\Query\SqlOrder;
 use function array_fill_keys;
 use function array_key_exists;
 
-class SearchOrder
+class SearchOrder extends SqlOrder
 {
     /** @var array<array<string,string>> */
     public $fields = [];
 
     public function __construct(array $fields)
     {
+        parent::__construct('custom', null);
+
         if (array_key_exists(0, $fields))
         {
             $fields = array_fill_keys($fields, 'desc');
